2ee4f0296b52ff0beb9396af88ead4410064df79
josephniski <jniski@wpi.edu>
2018-02-07 10:42:09 -0500

    added timer

--- main.c
+++ main.c
@@ -13,7 +13,6 @@ void configLaunchButtons();
 char buttonStates();
 
 
-
 // Declare globals here
 int state = 0;
 char pressed = 0xFF;
@@ -305,4 +304,19 @@ char buttonStates(){
     out = (out71|out72|out3|out2);
 
     return out;
+}
+
+void runtimerA2(void)
+{
+// This function configures and starts Timer A2
+// Timer is counting ~0.01 seconds
+//
+// Input: none, Output: none
+//
+// smj, ECE2049, 17 Sep 2013
+//
+// Use ACLK, 16 Bit, up mode, 1 divider
+    TA2CTL = TASSEL_1 + MC_1 + ID_0;
+    TA2CCR0 = 327; // 327+1 = 328 ACLK tics = ~1/100 seconds
+    TA2CCTL0 = CCIE; // TA2CCR0 interrupt enabled
 }
